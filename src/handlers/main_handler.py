3166618d05e03c6af3179db6ed6f56b7426625b3
lxzd-bxy <150052308+lxzd-bxy@users.noreply.github.com>
2025-03-31 21:24:54 +0500

    Main button fix

--- src/handlers/main_handler.py
+++ src/handlers/main_handler.py
@@ -3,9 +3,9 @@ from src.keyboards.main_keyboard import get_main_keyboard
 def register_main_handler(bot):
     @bot.callback_query_handler(func=lambda call: call.data in ["main", "back"])
     def show_main_menu(call):
-        bot.edit_message_text(
-            "Выберите одну из опций:", 
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        bot.send_message(
             call.message.chat.id, 
-            call.message.message_id,
+            "Выберите одну из опций:", 
             reply_markup=get_main_keyboard()
         )
--- src/keyboards/main_keyboard.py
+++ src/keyboards/main_keyboard.py
@@ -2,10 +2,11 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_main_keyboard():
     keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton("👤 Моя анкета", callback_data="my_profile"))
-    keyboard.add(InlineKeyboardButton("🔍 Смотреть анкеты", callback_data="search"))
-    keyboard.add(InlineKeyboardButton("Взаимные лайки", callback_data="reciprocal"))
-    keyboard.add(InlineKeyboardButton("Лайки", callback_data="likes"))
+    profile = InlineKeyboardButton("👤 Моя анкета", callback_data="my_profile")
+    search = InlineKeyboardButton("🔍 Смотреть анкеты", callback_data="search")
+    reciprocal = InlineKeyboardButton("Взаимные лайки", callback_data="reciprocal")
+    likes = InlineKeyboardButton("Лайки", callback_data="likes")
+    keyboard.add(profile, search, reciprocal, likes)
     return keyboard
 
 def get_back_to_menu_keyboard():
